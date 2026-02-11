80019524fc223bba7875a20c7766b4ac6baa53de
Paul Alaterre <alaterre.paul9@gmail.com>
2026-02-11 13:49:51 +0100

    Update Opcode.java

--- redcode/Opcode.java
+++ redcode/Opcode.java
@@ -1,12 +1,12 @@
-package corewar.redcode;
+package redcode;
 
 
 public enum Opcode {
     MOV,  // copie une valeur / instruction
     ADD,  // addition
-    SUB,  // soustraction
     JMP,  // saut
     DAT   // donnée / tue le process
 
     
 }
+
