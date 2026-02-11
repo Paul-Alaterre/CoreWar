5860ef7116d2017900a29c19720337688fe592f5
Paul Alaterre <alaterre.paul9@gmail.com>
2026-02-11 13:50:48 +0100

    Update Instruction.java

--- redcode/Instruction.java
+++ redcode/Instruction.java
@@ -1,81 +1,55 @@
-package corewar.redcode;
-
-import corewar.mars.Mars;
-import corewar.mars.Memory;
-import corewar.mars.Process;
+package redcode;
 
 public class Instruction {
-    public Opcode opcode;
-    public Mode modeA;
-    public Mode modeB;
-    public int a;
-    public int b;
 
-    public Instruction(Opcode opcode, Mode modeA, int a, Mode modeB, int b) {
+    private Opcode opcode;
+
+    private Mode modeA;
+    private int valueA;
+
+    private Mode modeB;
+    private int valueB;
+
+    public Instruction(Opcode opcode,
+                       Mode modeA, int valueA,
+                       Mode modeB, int valueB) {
         this.opcode = opcode;
         this.modeA = modeA;
-        this.a = a;
+        this.valueA = valueA;
         this.modeB = modeB;
-        this.b = b;
+        this.valueB = valueB;
     }
 
-    @Override
-    public String toString() {
-        return opcode + " " + modeA + " " + a + ", " + modeB + " " + b;
+    public int getA(){
+        return this.valueA;
     }
 
-
-                                               /* EXECUTE */
-
-        public void execute(Mars mars, Process p) {
-    Memory mem = mars.getMemory();
-
-    switch (opcode) {
-
-        case DAT ->                                    // Le process meurt immédiatement
-            mars.killProcess(p);
-
-        case JMP ->                                    // Saut direct : pc = pc + a
-            p.pc = Math.floorMod(p.pc + a, mem.size());
-
-        case MOV -> {
-            // Copie l'instruction à l'adresse (pc + a) vers (pc + b)
-            
-            Instruction src = mem.get(p.pc + a);
-            Instruction copy = new Instruction(
-                    src.opcode, src.modeA, src.a, src.modeB, src.b
-            );
-            mem.set(p.pc + b, copy);
-            p.pc = Math.floorMod(p.pc + 1, mem.size());
-            }
-
-        case ADD -> {
-            // Ajoute la valeur A au champ B de l'instruction cible
-            Instruction target = mem.get(p.pc + b);
-            if (target == null) {
-                mars.killProcess(p);
-                return;
-            }
-
-            
-            Instruction newInst = new Instruction(
-                    target.opcode,
-                    target.modeA,
-                    target.a,
-                    target.modeB,
-                    target.b + this.a
-            );
-            mem.set(p.pc + b, newInst);
-            p.pc = Math.floorMod(p.pc + 1, mem.size());
-            }
-
-        default -> // Pour l'instant, rien d'autre
-            p.pc = Math.floorMod(p.pc + 1, mem.size());
+    public int getB(){
+        return this.valueB;
     }
 
-    if (opcode == null) {
-        mars.killProcess(p);
+    public Opcode getOpcode() {
+    return opcode;
     }
-}
+
+    public Mode getModeA() {
+        return modeA;
+    }
+
+    public Mode getModeB() {
+        return modeB;
+    }
+
+    public void setB(int valueB) {
+        this.valueB = valueB;
+    }
+
+    public Instruction copy() {
+        return new Instruction(
+            opcode,
+            modeA, valueA,
+            modeB, valueB
+        );
+    }
 
 }
