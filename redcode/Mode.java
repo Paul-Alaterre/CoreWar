129d01bd1c8ac512c1d933fefed5c50ad07f2b72
Paul Alaterre <alaterre.paul9@gmail.com>
2026-02-12 15:42:18 +0100

    Update Mode.java

--- redcode/Mode.java
+++ redcode/Mode.java
@@ -1,17 +1,20 @@
 package redcode;
-
+/**C'est la classe qui définit les différents modes d'adressages*/
 public enum Mode {
     IMMEDIATE("#"),
     DIRECT(""),
     INDIRECT("@");
 
+    /**Variable qui est le symbole associé au mode*/
     private final String symbol;
 
+    /**Le constructeur qui associe le symbole au au mode*/
     Mode(String symbol) {
         this.symbol = symbol;
     }
-
+    /**Une méthode pour récupérer le caractère associé au mode*/
     public String symbol() {
         return symbol;
     }
+
 }
