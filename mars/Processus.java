a34fb2cf82208b9980cfe5c61ab53e0fb20ca051
Paul Alaterre <alaterre.paul9@gmail.com>
2026-02-12 15:25:17 +0100

    adding comments

--- mars/Processus.java
+++ mars/Processus.java
@@ -1,24 +1,39 @@
 package mars;
 import redcode.Instruction;
 
-public class Processus {
+/**
+* C'est la classe représentant un processus, c'est à dire un programme qui interagit 
+* avec la mémoire en éxecutant les
+* instructions contenus dans celle-ci.
+*/
 
+public class Processus {
+    /**C'est le pointeur du processus, celui qui définit sa position dans la mémoire*/
     private int pc; //program counter
 
+    /**Le constructeur qui prend un indice pour initialiser le pointeur*/
     public Processus(int pc) {
         this.pc = pc;
     }
 
+    /**Un accesseur pour connaître la position du processus*/
     public int getPc(){
         return this.pc;
     }
-    
+
+    /**Une méthode pour redéfinir la position du processus*/
     public void setPc(int pc, int memSize){
         this.pc = Math.floorMod(pc, memSize);
     }
 
+    /**Une méthode pour faire avancer le processus d'un certain 
+    * nombre de cases (peu être à supprimer car on peut faire la même 
+    * chose avec setPC
+    */
+    
     public void advance(int n, int memSize) {
         this.pc = Math.floorMod(this.pc + n, memSize);
     }
+
 
 }
