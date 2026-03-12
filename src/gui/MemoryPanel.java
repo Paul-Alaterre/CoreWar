90e2a5792ae73d47e3893e91eb958774b2490668
Thierno Sadou Diallo <thiernosadoudiallo356@gmail.com>
2026-03-12 01:36:16 +0100

    Enhance MemoryPanel with visited cells tracking
    Added functionality to clear visited cells and update painting logic to include visited cells in the MemoryPanel.

--- src/gui/MemoryPanel.java
+++ src/gui/MemoryPanel.java
@@ -1,22 +1,29 @@
 package gui;
 
+import game.CorewarGame;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
 import javax.swing.*;
 import mars.Processus;
 import mars.Warrior;
-import game.CorewarGame;
-
-import java.awt.*;
 
 public class MemoryPanel extends JPanel{
     private CorewarGame game; // Pour accéder aux processus des Warriors
     private final int CELL_SIZE = 8; // Taille d'une case en pixels
     private final int COLUMNS = 80;
+    private Set<Integer> visitedCells = new HashSet<>();
 
     public MemoryPanel(CorewarGame game){
         this.game = game;
         int rows = game.getMemory().getSize() / COLUMNS;
         setPreferredSize(new Dimension(COLUMNS * CELL_SIZE, rows * CELL_SIZE));
     }
+
+    public void clearTraces() {
+    visitedCells.clear();
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -38,6 +45,21 @@ public class MemoryPanel extends JPanel{
                 g.setColor(getWarriorColor(w.getId())); // On repasse à la couleur du guerrier
             }
         }
+
+        for (Warrior w : game.getWarriors()) {
+            for (Processus p : w.getProcessQueue()) {
+                 visitedCells.add(p.getPc());
+
+                g.setColor(new Color(120, 200, 255)); // bleu ciel
+                 for (int addr : visitedCells) {
+                    int x = (addr % COLUMNS) * CELL_SIZE;
+                    int y = (addr / COLUMNS) * CELL_SIZE;
+                    g.fillRect(x, y, CELL_SIZE - 1, CELL_SIZE - 1);
+                }
+ 
+            }
+}
+
         
     }
 
